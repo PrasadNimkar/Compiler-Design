41282013c3827aa6d75e71ad8c45c975cee52761
Prasad Nimkar <83298007+PrasadNimkar@users.noreply.github.com>
2022-10-21 09:49:55 +0530

    Update ex_3.cpp

--- ex_3.cpp
+++ ex_3.cpp
@@ -1,3 +1,59 @@
+/* Aim:Implement a program to check paranthesis of regular expression is balanced or not.*/
+#include <bits/stdc++.h>
+using namespace std;
+bool areBracketsBalanced(string expr)
+{
+    stack<char> s;
+    char x;
+    for (int i = 0; i < expr.length(); i++)
+    {
+        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{')
+        {
+            s.push(expr[i]);
+            continue;
+        }
+        if (s.empty())
+            return false;
+
+        switch (expr[i])
+        {
+        case ')':
+            x = s.top();
+            s.pop();
+            if (x == '{' || x == '[')
+                return false;
+            break;
+        case '}':
+            x = s.top();
+            s.pop();
+            if (x == '(' || x == '[')
+                return false;
+            break;
+
+        case ']':
+            x = s.top();
+            s.pop();
+            if (x == '(' || x == '{')
+                return false;
+            break;
+        }
+    }
+    return (s.empty());
+}
+int main()
+{
+    string expr = "{()}[]";
+    if (areBracketsBalanced(expr))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+    return 0;
+}
+/*Output:
+Balanced
+*/
+
+/*
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -46,3 +102,4 @@ main()
     else
         cout << "Not Balanced";
 }
+*/
